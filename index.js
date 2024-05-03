8b8c49fc0fd2e9f2a3fa44da4cd1cf1c3d030d25
bghbmh <oooonew@naver.com>
2024-05-03 17:45:25 +0900

    asd

--- index.js
+++ index.js
@@ -1,79 +1,82 @@
-const express = require('express');
-const fs = require('fs')
-var cors = require('cors');
-const app = express();
+console.log("index.js workflows")
 
-const path = require('path');
-const publicPath = path.join(__dirname, 'public');
-//app.use(express.static(publicPath));
-app.use(express.static('./'));
-app.use(cors());
 
-const layout = require('./components/layout.js');
+// const express = require('express');
+// const fs = require('fs')
+// var cors = require('cors');
+// const app = express();
 
-const multer = require('multer');
+// const path = require('path');
+// const publicPath = path.join(__dirname, 'public');
+// //app.use(express.static(publicPath));
+// app.use(express.static('./'));
+// app.use(cors());
+
+// const layout = require('./components/layout.js');
+
+// const multer = require('multer');
+// // const upload = multer({
+// // 	dest: 'files/'
+// // });
 // const upload = multer({
-// 	dest: 'files/'
+//     storage: multer.diskStorage({
+//       	filename(req, file, done) {
+//           	//console.log("multer filename - ", file);
+// 			done(null, file.originalname);
+//         },
+// 		destination(req, file, done) {
+//       		//console.log("multer destination - ", file);
+// 		    //done(null, path.join(__dirname, "public"));
+// 			done(null, path.join(__dirname, "files"));
+// 	    },
+//     }),
+// 	//limits: { fileSize: 1024 * 1024 }
 // });
-const upload = multer({
-    storage: multer.diskStorage({
-      	filename(req, file, done) {
-          	//console.log("multer filename - ", file);
-			done(null, file.originalname);
-        },
-		destination(req, file, done) {
-      		//console.log("multer destination - ", file);
-		    //done(null, path.join(__dirname, "public"));
-			done(null, path.join(__dirname, "files"));
-	    },
-    }),
-	//limits: { fileSize: 1024 * 1024 }
-});
 
-//const uploadMiddleware = upload.single('mainimgFile');
-//const uploadMiddleware = upload.array("subimgFile");
-const uploadMiddleware = upload.fields([
-	{ name: "mainimgFile" },
-	{ name: "subimgFile" }
-]);
+// //const uploadMiddleware = upload.single('mainimgFile');
+// //const uploadMiddleware = upload.array("subimgFile");
+// const uploadMiddleware = upload.fields([
+// 	{ name: "mainimgFile" },
+// 	{ name: "subimgFile" }
+// ]);
 
 
-app.use((err, req, res, next) => {
-	console.log("error middleware");
-	console.log(err.toString());
-	res.send(err.toString());
-});
+// app.use((err, req, res, next) => {
+// 	console.log("error middleware");
+// 	console.log(err.toString());
+// 	res.send(err.toString());
+// });
 
-app.use(uploadMiddleware);
-app.use(express.json());
+// app.use(uploadMiddleware);
+// app.use(express.json());
 
-app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
 
-	//response.send( layout.adminTest11("test title") );
-	res.send(layout.adminMain("test title", '') )
-});
+// 	//response.send( layout.adminTest11("test title") );
+// 	res.send(layout.adminMain("test title", '') )
+// });
 
-app.post('/upload', (req, res) => {
-    console.log("upload - ", req.body );
-	//console.log("upload files - ", req.files );
+// app.post('/upload', (req, res) => {
+//     console.log("upload - ", req.body );
+// 	//console.log("upload files - ", req.files );
 
-	fs.readFile('./data/test.txt', 'utf8', (err, data) => {
-		console.log(data);
-	});
-
-
-	const fileData = fs.readFileSync('./data/myList.json', 'utf8');
-	let temp = JSON.parse(fileData);
-	req.body.id = temp.length + 1;
-	temp.push(req.body);
-
-	fs.writeFileSync('./data/myList.json', JSON.stringify(temp));
+// 	fs.readFile('./data/test.txt', 'utf8', (err, data) => {
+// 		console.log(data);
+// 	});
 
 
-    res.status(200).send('test uploaded');
-});
+// 	const fileData = fs.readFileSync('./data/myList.json', 'utf8');
+// 	let temp = JSON.parse(fileData);
+// 	req.body.id = temp.length + 1;
+// 	temp.push(req.body);
+
+// 	fs.writeFileSync('./data/myList.json', JSON.stringify(temp));
 
 
-app.listen(3300, () => {
-    console.log('server is running at 3000');
-});
+//     res.status(200).send('test uploaded');
+// });
+
+
+// app.listen(3300, () => {
+//     console.log('server is running at 3000');
+// });
